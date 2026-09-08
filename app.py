b617beedf16b5fd577dd649721b095b024e72006
janeshl <74814793+janeshl@users.noreply.github.com>
2026-09-08 14:58:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def finalize():
     return jsonify(ok=True, human=human_score, ai=b["score"], ai_strategy=b)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)), debug=False)
